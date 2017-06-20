2dff5bba5d5bf9e6180c7c651003dbc92066b085
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-20 06:46:48 -0400

    Primer commit

---
+++ basic-Swift-app/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  basic-Swift-app
+//
+//  Created by Fredwin Rosales on 6/20/17.
+//  Copyright © 2017 fredwinrosales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var outputLabel2: UILabel!
+    
+    var currentCount : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addOneButton(_ sender: UIButton) {
+        currentCount = currentCount + 1
+        outputLabel2.text = "The button has been clicked \(currentCount) number of times"
+        outputLabel2.textColor = UIColor.red
+    }
+    
+
+}
+
