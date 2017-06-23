6959529c622702c100ac74b51a5e40f8de77ad03
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-22 20:57:42 -0400

    Primer storyboard
    Se crea storyboard y se aplican eventos en botones para mostar texto estático en consola

---
+++ basic-Swift-app/vcMain.swift
@@ -0,0 +1,37 @@
+//
+//  vcMain.swift
+//  basic-Swift-app
+//
+//  Created by Fredwin Rosales on 6/22/17.
+//  Copyright © 2017 fredwinrosales. All rights reserved.
+//
+
+import UIKit
+
+class vcMain: UIViewController {
+
+    @IBOutlet var txtUsername: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    
+    @IBAction func btnSave()
+    {
+        print("Save Button Pressed")
+    }
+    
+    @IBAction func btnLoad()
+    {
+        print("Load Button Pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
