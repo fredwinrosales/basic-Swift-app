42ff7df72c4e4558b618b7f76d00bf3c412f0fbf
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-24 23:27:20 -0400

    Lista contenido
    Se lista contenido de arreglo en pantalla

--- basic-Swift-app/TableViewController.swift
+++ basic-Swift-app/TableViewController.swift
@@ -10,30 +10,24 @@ import UIKit
 
 var pets = ["dog", "cat", "rabbit"]
 var petsDesc = ["It is dog", "It is cat", "It is rabbit"]
+var hola = "Hola mundo"
 
 class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return pets.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = pets[indexPath.row]
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
