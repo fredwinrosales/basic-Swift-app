dd46b797cc31fc70500329a7a995709a67f7fdf1
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-24 22:56:46 -0400

    Array data

--- basic-Swift-app/TableViewController.swift
+++ basic-Swift-app/TableViewController.swift
@@ -8,22 +8,10 @@
 
 import UIKit
 
+var pets = ["dog", "cat", "rabbit"]
+var petsDesc = ["It is dog", "It is cat", "It is rabbit"]
+
 class TableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
